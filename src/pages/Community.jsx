b2726452962923d1b85bc58d2e3855144370fb37
CaioVitor1 <caiovitor33@hotmail.com>
2022-10-07 16:08:08 -0300

    refactor: create community component

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,8 +1,7 @@
-import styled from "styled-components";
 import Title from "../components/TitleComponent";
 import jobs from "../assets/images/jobs.jpg"
 import { useState } from "react";
-
+import {CommunityBody, UserProfile, UserProfileInfos, Users} from "../components/CommunityComponent"
 function ListUser({name, genres, book, author}) {
 return (
 <Users>
@@ -49,56 +48,3 @@ export default function Community(){
         </>
     )
 }
-
-const CommunityBody = styled.div`
-h2{
-font-size: 30px;
-color: #8b4513;
-width: 90%;
-display: flex;
-justify-content: center;
-align-items: center;
-}
-
-`
-
-const Users = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-margin-top: 30px;
-
-`
-
-const UserProfile = styled.div`
-background-color: #dcdcdc;
-width: 60%;
-border-radius: 20px;
-display: flex;
-
-img{
-width: 30vh;
-height: 30vh;
-border-radius: 100px;
-}
-`
-
-const UserProfileInfos = styled.div`
-margin-top:10px;
-display: flex;
-flex-direction: column;
-align-items: center;
-h3{
-font-size: 25px;
-margin-bottom: 10px;
-}
-h4{
-font-size:20px;
-}
-h5{
-margin-top: 60px;
-color:blue;
-font-size: 15px;
-}
-`
